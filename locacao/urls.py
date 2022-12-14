c64db95d6aba962ef9c8a98c6e3002926230aa4a
Bruno Irineu <irineubruno@hotmail.com>
2022-12-14 07:19:06 -0400

    v.3

--- locacao/urls.py
+++ locacao/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-path('', IndexView, name='index'),
+path('', IndexView.as_view(), name='index'),
 path('listarveiculos/', ListarVeiculosListView, name='listarveiculos'),
+path('listarpdf/', ListaVeiculosPdf, name='listarpdf'),
 
 ]
--- locacao/views.py
+++ locacao/views.py
@@ -4,6 +4,8 @@ from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 
 from locacao.models import Veiculo
+from locacao.utils import GeraPDFMixin
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -11,4 +13,14 @@ class IndexView(TemplateView):
 class ListarVeiculosListView(ListView):
     model = Veiculo
     template_name = 'listarveiculos.html'
+    queryset = Veiculo.objects.all()
     context_object_name = 'veiculos'
+
+class ListaVeiculosPdf(View, GeraPDFMixin):
+    def get(self, request, *args, **kwargs):
+        funcs = Veiculo
+        context = {
+            'veiculos': funcs,
+            'quant': funcs.count()
+        }
+        return self.render_to_pdf('listarpdf.html', context)
